91260791ba3003dfeba9a9340ca01bde01880152
Elena GRONDIN <elena.grondin2022@campus-eni.fr>
2024-03-12 16:15:23 +0100

    fix select

--- src/main/java/tp/tva/controller/TvaController.java
+++ src/main/java/tp/tva/controller/TvaController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.text.DecimalFormat;
+
 @Controller
 // @RequestMapping : permet de définir un préfixe d'url sur toutes les méthdoes du controller @GetMapping / @PostMapping
 @RequestMapping("/tva")
@@ -25,8 +27,9 @@ public class TvaController {
        // System.out.println("Taux TVA : " + taux);
 
         System.out.println("PrixTTC : " + prixHT + prixHT*taux);
+        double ttc = (prixHT + prixHT*taux/100);
 
-        model.addAttribute("ttc",  (prixHT + prixHT*taux/100) + " €");
+        model.addAttribute("ttc",  new DecimalFormat("0.00").format(ttc) + " €");
 
         return "tva";
     }
